df1082c6e95506deac33699f1c9c87cf9aa08662
BunyanXd <101381544+BunyanXd@users.noreply.github.com>
2022-03-23 13:35:38 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,9 +3,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ GoPay: 085730903853 [AlyaXd]
-├ Dana: 082221792667 [BOTCAHX]
-├ Pulsa: 081395861695 [BOTCAHX]
+├ GoPay: 089502435112 [AlyaXd]
+├ Dana: 083863605242 [BUNYAN]
+├ Pulsa: 083863605242 [BUNYAN]
 └────
 `.trim(), watermark, 'Menu', '.menu', m)
 handler.help = ['donasi']
